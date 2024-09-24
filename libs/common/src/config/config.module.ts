b1184f9e8314680bb5e79d9517df17b99d5556d8
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-09-25 04:32:03 +0700

    Update config.module.ts

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -8,8 +8,13 @@ import * as Joi from 'joi';
 @Module({
   imports: [
     NestConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
+        AUTH_PORT: Joi.number().required(),
+        VEHICLE_PORT: Joi.number().required(),
         MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
       }),
     }),
   ],
