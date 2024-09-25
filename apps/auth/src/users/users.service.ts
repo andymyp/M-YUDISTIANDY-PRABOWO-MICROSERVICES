03ca4047042ada4bd8e975c4847903315c706b2c
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-09-25 08:33:31 +0700

    validate duplicate email, accountNumber, identityNumber

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { UsersRepository } from './user.repository';
 import { CreateUserDTO } from './dto/create-user.dto';
 import * as bcryptjs from 'bcryptjs';
@@ -8,10 +8,49 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDTO) {
+    await this.validateEmail(createUserDto);
+    await this.validateAccountNumber(createUserDto);
+    await this.validateIdentityNumber(createUserDto);
     return this.usersRepository.create({
       ...createUserDto,
       password: await bcryptjs.hash(createUserDto.password, 10),
     });
+  }
+
+  async validateAccountNumber(createUserDto: CreateUserDTO) {
+    try {
+      await this.usersRepository.findOne({
+        accountNumber: createUserDto.accountNumber,
+      });
+    } catch (error) {
+      return;
+    }
+
+    throw new UnprocessableEntityException('accountNumber already exists');
+  }
+
+  async validateEmail(createUserDto: CreateUserDTO) {
+    try {
+      await this.usersRepository.findOne({
+        emailAddress: createUserDto.emailAddress,
+      });
+    } catch (error) {
+      return;
+    }
+
+    throw new UnprocessableEntityException('emailAddress already exists');
+  }
+
+  async validateIdentityNumber(createUserDto: CreateUserDTO) {
+    try {
+      await this.usersRepository.findOne({
+        identityNumber: createUserDto.identityNumber,
+      });
+    } catch (error) {
+      return;
+    }
+
+    throw new UnprocessableEntityException('emailAddress already exists');
   }
 
   async getUserByEmail(emailAddress: string) {
